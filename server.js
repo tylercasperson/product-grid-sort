eb25a655b1ba488bf59821b64fde0cb72c87cca6
Tyler Casperson <tylercasperson@gmail.com>
2020-09-24 10:11:23 -0600

    made a variable to determine if it is development or production in the server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const db = require('./models');
 const productsRoute = require('./routes/products');
+const path = require('path');
 
 // sets up express to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -17,6 +18,15 @@ app.use(cors());
 
 // routes
 app.use(productsRoute);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 // Warning handler
 process.on('warning', (warning) => {
